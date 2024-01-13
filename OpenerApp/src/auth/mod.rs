ce4a3a3ba32d634edbd97b4f1a0d5c87ed6679e6
FizzyApple12 <davideroeder@gmail.com>
2024-01-13 07:03:28 -0500

    data reading test

--- OpenerApp/src/auth/mod.rs
+++ OpenerApp/src/auth/mod.rs
@@ -14,12 +14,12 @@ pub fn auth_entry(gui_sender: Sender<i32>) {
     // let mut next_iter_resets = false;
 
     let mut context = nfc1::Context::new().unwrap();
-    let mut device = context.open_with_connstring(NFC_SERIAL).unwrap();
+    let mut device = context.open().unwrap();
 
     let _ = device.initiator_init();
     let _ = device.set_property_bool(nfc1::Property::InfiniteSelect, true);
 
-    'findtarget: loop {
+    loop {
         match device.initiator_select_passive_target(&nfc1::Modulation {
             modulation_type: nfc1::ModulationType::Iso14443a,
             baud_rate: nfc1::BaudRate::Baud106,
@@ -30,6 +30,12 @@ pub fn auth_entry(gui_sender: Sender<i32>) {
                 if let nfc1::target_info::TargetInfo::Iso14443a(target_info) = target.target_info {
                     //println!("Found ISO/IEC 14443-A target: {}", HexFmt(&target_info.uid[..target_info.uid_len]));
                     
+                    let _ = device.set_property_bool(nfc1::Property::EasyFraming, true);
+
+                    let test = device.initiator_transceive_bytes_timed([0x30, 0x00].as_mut_ptr(), 2).unwrap();
+
+                    println!("mifare data: {:?}", test);
+
                     let verified = true;
 
                     let _ = gui_sender.send(if verified {2} else {3});
@@ -37,8 +43,6 @@ pub fn auth_entry(gui_sender: Sender<i32>) {
                     thread::sleep(Duration::from_millis(5000));
 
                     let _ = gui_sender.send(0);
-
-                    break 'findtarget;
                 }
             }
             Err(_) => {}
