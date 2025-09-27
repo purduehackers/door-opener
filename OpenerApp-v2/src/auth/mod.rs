e2c2c2fbf5e2d6553c4361b17e8b2dfaa04fe1f2
Eric Park <me@ericswpark.com>
2025-09-27 01:59:21 -0400

    fix: temp print out errors

--- OpenerApp-v2/src/auth/mod.rs
+++ OpenerApp-v2/src/auth/mod.rs
@@ -60,6 +60,8 @@ pub fn check_passport_validity(id: i32, secret: String) -> Result<bool, ()> {
                 return Ok(true);
             }
             _ => {
+                println!("Got error status: {}", response.status());
+                println!("Got error text: {}", response.text().unwrap());
                 return Ok(false);
             }
         },
