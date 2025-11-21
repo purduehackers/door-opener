027ab3877fcd316cb0f816f376b1b06ce1fe99a0
Eric Park <me@ericswpark.com>
2025-11-21 18:33:08 -0500

    fix: remove unused NFC_SERIAL config variable
    Why is this even here???

--- src/config.rs
+++ src/config.rs
@@ -2,5 +2,3 @@ pub const DOOR_SERVO_SERIAL: &str = "/dev/ttyUSB0";
 pub const DOOR_SERVO_RELEASED_POSITION: u16 = 0;
 pub const DOOR_SERVO_PRESSED_POSITION: u16 = 1000;
 pub const DOOR_SERVO_ID: u8 = 1;
-
-pub const NFC_SERIAL: &str = "/dev/spidev0.0";
