7b9e7b0aa079714e014ec43f30b79ff6be205dc4
Eric Park <me@ericswpark.com>
2026-01-26 12:03:40 -0500

    fix: add integer values to the enums for now
    We can remove them once we've gotten rid of all the magic
    numbers in the code

--- src/enums.rs
+++ src/enums.rs
@@ -1,10 +1,10 @@
 #[derive(Debug, Clone, Copy, Eq, PartialEq, Default)]
 pub enum AuthState {
     #[default]
-    Idle,     // 0
-    Pending,  // 1
-    Valid,    // 2
-    Invalid,  // 3
-    NetError, // 4
-    NFCError, // 5
+    Idle = 0,
+    Pending = 1,
+    Valid = 2,
+    Invalid = 3,
+    NetError = 4,
+    NFCError = 5,
 }
