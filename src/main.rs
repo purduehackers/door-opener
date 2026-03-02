75193307f67390b3bef6ace011b3afe6d1fc230b
Eric Park <me@ericswpark.com>
2026-03-02 09:12:50 -0500

    fix: retry websocket connection on fail
    Fixes #16

--- src/main.rs
+++ src/main.rs
@@ -69,50 +69,52 @@ async fn ws_entry(opener_tx: UnboundedSender<()>) {
         Open,
     }
 
-    let (socket, _resp) =
-        match connect_async("wss://api.purduehackers.com/phonebell/door-opener").await {
-            Ok(x) => x,
-            Err(e) => {
-                eprintln!("Failed to connect to API WebSocket: {e}");
-                return;
-            }
-        };
-
-    let (mut write, mut read) = socket.split();
-
-    write
-        .send(Message::Text(
-            env::var("DOOR_OPENER_API_KEY")
-                .expect("door opener API key")
-                .into(),
-        ))
-        .await
-        .expect("write auth");
-
     loop {
-        tokio::select! {
-            () = sleep(Duration::from_secs(25)) => {
-                write.send(Message::Ping(Bytes::default())).await.expect("ping");
-            }
-            msg = read.next() => {
-                match msg {
-                    Some(Ok(Message::Text(t))) => {
-                        if let Ok(msg) = serde_json::from_str(t.as_ref()) {
-                            match msg {
-                                WebSocketMessage::Open => {
-                                    let _ = opener_tx.send(());
+        let (socket, _resp) =
+            match connect_async("wss://api.purduehackers.com/phonebell/door-opener").await {
+                Ok(x) => x,
+                Err(e) => {
+                    eprintln!("Failed to connect to API WebSocket: {e}");
+                    return;
+                }
+            };
+
+        let (mut write, mut read) = socket.split();
+
+        write
+            .send(Message::Text(
+                env::var("DOOR_OPENER_API_KEY")
+                    .expect("door opener API key")
+                    .into(),
+            ))
+            .await
+            .expect("write auth");
+
+        loop {
+            tokio::select! {
+                () = sleep(Duration::from_secs(25)) => {
+                    write.send(Message::Ping(Bytes::default())).await.expect("ping");
+                }
+                msg = read.next() => {
+                    match msg {
+                        Some(Ok(Message::Text(t))) => {
+                            if let Ok(msg) = serde_json::from_str(t.as_ref()) {
+                                match msg {
+                                    WebSocketMessage::Open => {
+                                        let _ = opener_tx.send(());
+                                    }
                                 }
                             }
                         }
+                        Some(Ok(Message::Ping(_) | Message::Pong(_))) => {},
+                        Some(Err(e)) => eprintln!("Received err: {e:?}"),
+                        None => break,
+                        _ => eprintln!("Unsupported message received! {msg:?}"),
                     }
-                    Some(Ok(Message::Ping(_) | Message::Pong(_))) => {},
-                    Some(Err(e)) => eprintln!("Received err: {e:?}"),
-                    None => break,
-                    _ => eprintln!("Unsupported message received! {msg:?}"),
                 }
             }
         }
-    }
 
-    println!("WebSocket connection closed.");
+        println!("WebSocket connection closed.");
+    }
 }
